56ba5d020896998fbb508e66d42ed8ae351a0eda
Ryan Goh <ryangohce@gmail.com>
2023-01-16 14:25:06 +0800

    Modularized the fetchBTCMYR, convRates, binanceUSD and calc functions

--- index.js
+++ index.js
@@ -1,52 +1,34 @@
-import Binance from 'node-binance-api' //import instead of require for .mjs file
-const client = Binance()
+import { fetchBTCMYR } from './lib/luno.js'
+import convRates from './lib/conv.js'
+import binanceUSD from './lib/binance.js'
+import { calc } from './lib/calculations.js'
 import * as dotenv from 'dotenv' //import instead of require for .mjs file
 dotenv.config()
 
 //Retrieve BTCMYR data from Luno 
-async function fetchBTCMYR(){
-  const response = await fetch("https://api.luno.com/api/1/ticker?pair=XBTMYR")
-  const BTCMYR = await response.json()
-  return BTCMYR['bid']
-}
-
 const btcLunoMYR = await fetchBTCMYR()
-console.log(btcLunoMYR)
+// console.log(btcLunoMYR)
 
-//Retrieve USDMYR conversion rate 
-async function fetchConv(){
-  let myHeaders = new Headers();
-  myHeaders.append("apikey", process.env.API_KEY);
-
-  let requestOptions = {
-    method: 'GET',
-    redirect: 'follow',
-    headers: myHeaders
-  };
-
-  const response = await fetch("https://api.apilayer.com/fixer/latest?symbols=MYR&base=USD", requestOptions)
-  const USDMYR = await response.json()
-  return USDMYR.rates['MYR'] 
-}
-
-const convRate = await fetchConv()
-//console.log(convRate)
+// //Retrieve USDMYR conversion rate 
+let convRate = await convRates(process.env.API_KEY)
+// console.log(convRate)
 
 
 //Call BTCUSD price from Binance 
-let test =await client.prices('BTCBUSD')
-let btcBinanceUSD = test.BTCBUSD
-//console.log(btcBinanceUSD);
+let btcBinanceUSD = await binanceUSD()
+// console.log(btcBinanceUSD);
 
 
 //Calculate BTCUSD price from Luno, price diff & premium 
-let btcLunoUSD = btcLunoMYR / convRate
-let priceDiff = Math.abs(btcLunoUSD - btcBinanceUSD)
-let percentDiff = (priceDiff/btcLunoUSD)*100
-percentDiff = percentDiff.toFixed(2)
-console.log("BTCMYR price on Luno:        MYR ", btcLunoMYR)
-console.log("USDMYR:                          ", convRate)
-console.log("BTCUSD price on Luno:        USD ", btcLunoUSD)
-console.log("BTCUSD price on Binance:     USD ", btcBinanceUSD)
-console.log("Price difference:            USD ", priceDiff)
-console.log("Luno premium:                    ", percentDiff, "%")
+let cryptMath = await calc(btcLunoMYR, convRate, btcBinanceUSD)
+// console.log(cryptMath.btcLunoUSD)
+// console.log(cryptMath.priceDiff)
+// console.log(cryptMath.percentDiff)
+
+//Print values
+console.log("BTCMYR price on Luno:".padEnd(40, ' ') + "MYR ", btcLunoMYR)
+console.log("USDMYR:".padEnd(44, ' '), convRate)
+console.log("BTCUSD price on Luno:".padEnd(40, ' ') + "USD ", cryptMath.btcLunoUSD)
+console.log("BTCUSD price on Binance:".padEnd(40, ' ') + "USD ", btcBinanceUSD)
+console.log("Price difference:".padEnd(40, ' ') + "USD ", cryptMath.priceDiff)
+console.log("Luno premium:".padEnd(44, ' '), cryptMath.percentDiff, "%")
