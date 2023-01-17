1ff3af03515b36358f969b65bd8f083367192264
Ryan Goh <ryangohce@gmail.com>
2023-01-17 10:55:10 +0800

    change binance.test.js to test for successful and unsuccesful retrieval of price

--- test/binance.test.js
+++ test/binance.test.js
@@ -2,19 +2,41 @@ beforeEach(() => {
     jest.resetModules() //reset module mocks before each test
 })
 
-const MOCK_PRICE = 20000
-const MOCK_JSON_RESP = { BTCBUSD: MOCK_PRICE }
+// const MOCK_PRICE = 20000
+// const MOCK_JSON_RESP = { BTCBUSD: MOCK_PRICE }
 
-import getBinancePrice from '../lib/binance.js'
+//import getBinancePrice from '../lib/binance.js'
 
-jest.mock('node-binance-api', () => {
-    return class Binance {
-        prices() {
-            return new Promise(resolve => resolve(MOCK_JSON_RESP));
-        }
-    }
-})
+// jest.mock('node-binance-api', () => {
+//     return class Binance {
+//         prices() {
+//             return new Promise(resolve => resolve(MOCK_JSON_RESP));
+//         }
+//     }
+// })
 
 test("Returns Message for Successful Response", async () => {
+    const getBinancePrice = require('../lib/binance.js').default //for export defaults 
+    const MOCK_PRICE = 20000
+    const MOCK_JSON_RESP = { BTCBUSD: MOCK_PRICE }
+    jest.mock('node-binance-api', () => {
+        return class Binance {
+            prices() {
+                return new Promise(resolve => resolve(MOCK_JSON_RESP));
+            }
+        }
+    })
     expect(await getBinancePrice()).toBe(MOCK_PRICE);
+});
+
+test("Returns Message for Unsuccesful Response", async () => {
+    const getBinancePrice = require('../lib/binance.js').default //for export defaults 
+    jest.mock('node-binance-api', () => {
+        return class Binance {
+            prices() {
+                return new Promise(resolve => resolve("Failed to retrieve price"));
+            }
+        }
+    })
+    expect(await getBinancePrice()).toBe("Failed to retrieve price");
 });
