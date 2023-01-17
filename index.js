a1b515aff6f6a866757aa84acfa63671bcf7c16e
Ryan Goh <ryangohce@gmail.com>
2023-01-17 16:31:40 +0800

    Destructure calculations module output, implemented try-catch error checking for luno.js, conv.js and binance.js, and ensuring that output from luno.js, conv.js and binance.js are numbers

--- index.js
+++ index.js
@@ -7,20 +7,30 @@ dotenv.config()
 
 //Retrieve BTCMYR data from Luno 
 const btcLunoMYR = await fetchBTCMYR()
-//console.log(btcLunoMYR)
+if (typeof btcLunoMYR !== "number") {
+    throw 'Data retrieved from Luno is not a number'
+}
+//console.log(typeof btcLunoMYR)
 
 // //Retrieve USDMYR conversion rate 
 let convRate = await convRates(process.env.API_KEY)
+if (typeof convRate !== "number") {
+    throw 'Conversion rate retrieved is not a number'
+}
 //console.log(convRate)
 
 
 //Call BTCUSD price from Binance 
 let btcBinanceUSD = await binanceUSD()
+if (typeof btcBinanceUSD !== "number") {
+    throw 'Data retrieved from Binance is not a number'
+}
 //console.log(btcBinanceUSD);
 
 
 //Calculate BTCUSD price from Luno, price diff & premium 
-let cryptMath = await calc(btcLunoMYR, convRate, btcBinanceUSD)
+let { btcLunoUSD, priceDiff, percentDiff } = await calc(btcLunoMYR, convRate, btcBinanceUSD)
+//console.log(typeof percentDiff)
 // console.log(cryptMath.btcLunoUSD)
 // console.log(cryptMath.priceDiff)
 // console.log(cryptMath.percentDiff)
@@ -28,7 +38,7 @@ let cryptMath = await calc(btcLunoMYR, convRate, btcBinanceUSD)
 //Print values
 console.log("BTCMYR price on Luno:".padEnd(40, ' ') + "MYR ", btcLunoMYR)
 console.log("USDMYR:".padEnd(44, ' '), convRate)
-console.log("BTCUSD price on Luno:".padEnd(40, ' ') + "USD ", cryptMath.btcLunoUSD)
+console.log("BTCUSD price on Luno:".padEnd(40, ' ') + "USD ", btcLunoUSD)
 console.log("BTCUSD price on Binance:".padEnd(40, ' ') + "USD ", btcBinanceUSD)
-console.log("Price difference:".padEnd(40, ' ') + "USD ", cryptMath.priceDiff)
-console.log("Luno premium:".padEnd(44, ' '), cryptMath.percentDiff, "%")
+console.log("Price difference:".padEnd(40, ' ') + "USD ", priceDiff)
+console.log("Luno premium:".padEnd(44, ' '), percentDiff, "%")
--- test/conv.test.js
+++ test/conv.test.js
@@ -1,17 +1,26 @@
-const MOCK_RATE = 4.5
-const MOCK_JSON_RESP = {
-    rates:
-        { MYR: MOCK_RATE }
-}
-
-//Substitutes line 9 - line 10 into conv.js response object. Will only contain status and rates
-global.fetch = jest.fn(() => Promise.resolve({
-    status: 200,
-    json: () => Promise.resolve(MOCK_JSON_RESP)
-}))
-
 import convRate from '../lib/conv.js'
 
 test("Returns the correct conversion rate", async () => {
+    const MOCK_RATE = 4.5
+    const MOCK_JSON_RESP = {
+        rates:
+            { MYR: MOCK_RATE }
+    }
+    //Substitutes line 11 - line 12 into conv.js response object. Will only contain status and rates
+    global.fetch = jest.fn(() => Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve(MOCK_JSON_RESP)
+    }))
+
     expect(await convRate()).toBe(MOCK_RATE)
 })
+
+test("Returns an error in retrieving conversion rate", async () => {
+    const MOCK_STATUS_CODE = 500
+
+    global.fetch = jest.fn(() => Promise.resolve({
+        status: MOCK_STATUS_CODE,
+        json: () => { }
+    }));
+    expect(await convRate()).toBe("Failed to retrieve conversion rate");
+});
